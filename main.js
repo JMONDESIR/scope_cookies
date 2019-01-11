8dd09222848733cff59b1ec598539802b2886c46
JMONDESIR <Joey.M.Dev@gmail.com>
2019-01-11 10:28:16 -0600

    conjunction function added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//CODE WITH ERRORS
 // const cookies = [
 //   "Oatmeal Raisin",
 //   "Chocolate Chip",
@@ -13,6 +14,7 @@
 //   console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`);
 // }
 
+// CODE CORRECTED
 const cookies = [
   "Oatmeal Raisin",
   "Chocolate Chip",
@@ -27,3 +29,19 @@ for (let x = 1; x < cookies.length; x++) {
   const currentCookie = cookies[x];
   console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`);
 }
+
+// ORIGINAL CODE WITH SCOPE ERROR
+// const conjunction = function (firstWord, secondWord) {
+//   const conjoinedWord = `${firstWord} ${secondWord}`
+// }
+
+// conjunction("Master", "Card")
+// console.log(conjoinedWord)
+
+// SCOPE ERROR CORRECTED
+const conjunction = function(firstWord, secondWord) {
+  const conjoinedWord = `${firstWord} ${secondWord}`;
+  console.log(conjoinedWord);
+};
+
+conjunction("Master", "Card");
